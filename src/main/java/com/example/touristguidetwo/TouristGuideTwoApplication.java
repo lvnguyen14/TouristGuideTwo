79507f506e67d02dc04e8ff0f50262bf35b2c4fb
Laerkemarie <laje0007@stud.kea.dk>
2025-02-18 12:29:49 +0100

    test

---
+++ src/main/java/com/example/touristguidetwo/TouristGuideTwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.touristguidetwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristGuideTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TouristGuideTwoApplication.class, args);
+    }
+
+}
